2a75eb1835dc29401ddd1b3285d0940a1237dae8
Timo Mihaljov <timo.mihaljov@solita.fi>
2014-02-10 13:20:10 +0200

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cas-single-sign-out "0.1.0-SNAPSHOT"
+(defproject cas-single-sign-out "0.1.0"
   :description "Ring middleware for CAS single sign out"
   :url "https://github.com/solita/cas-single-sign-out"
   :license {:name "Eclipse Public License"
